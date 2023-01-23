7b1048d760fec212a9ceedaf6f94003e847aa2be
Yuna Tomida <ytomida.mmm@gmail.com>
2023-01-23 11:02:37 +0900

    add skeleton for clint/uart

--- modules/devices/clint.v
+++ modules/devices/clint.v
@@ -1,5 +1,18 @@
 module devices
 
+import consts
+import csr { State }
+
+const (
+	msip         = consts.clint_base
+	msip_end     = msip + 0x4
+	mtimecmp     = msip + 0x4000
+	mtimecmp_end = mtimecmp + 0x8
+	mtime        = msip + 0xbff8
+	mtime_end    = mtime + 0x8
+)
+
+// Core-local Interrupter
 pub struct Clint {
 mut:
 	msip     u32
@@ -7,10 +20,31 @@ mut:
 	mtime    u64
 }
 
-pub fn (c Clint) write(addr u64, value u64, size u8) ! {
+pub fn (mut c Clint) increment(mut state State) {
+	c.mtime += 1
+	if c.msip & 1 != 0 {
+	}
+	if c.mtimecmp > c.mtime {
+	} else {
+	}
+}
+
+pub fn (mut c Clint) write(addr u64, value u64, size u8) ! {
+	match true {
+		devices.msip <= addr && addr <= devices.msip_end {}
+		devices.mtimecmp <= addr && addr <= devices.mtimecmp_end {}
+		devices.mtime <= addr && addr <= devices.mtime_end {}
+		else {}
+	}
 	panic('TODO: Clint.write')
 }
 
 pub fn (c Clint) read(addr u64, size u8) !u64 {
+	match true {
+		devices.msip <= addr && addr <= devices.msip_end {}
+		devices.mtimecmp <= addr && addr <= devices.mtimecmp_end {}
+		devices.mtime <= addr && addr <= devices.mtime_end {}
+		else {}
+	}
 	panic('TODO: Clint.read')
 }
--- modules/devices/uart.v
+++ modules/devices/uart.v
@@ -1,11 +1,32 @@
 module devices
 
-const uart_size = 0x100
+import consts
+
+pub const size = 0x100
+
+const (
+	// Receive holding register (for input bytes)
+	rhr    = consts.uart_base
+	// Transmit holding register (for input bytes)
+	thr    = consts.uart_base
+	// Interrupt enabling register
+	ier    = consts.uart_base + 1
+	// FIFO controll register
+	fcr    = consts.uart_base + 2
+	// Interrupt status register
+	isr    = consts.uart_base + 2
+	// Line control register
+	lcr    = consts.uart_base + 3
+	// Line status register
+	lsr    = consts.uart_base + 5
+	lsr_rx = u8(1)
+	lsr_tx = u8(1 << 5)
+)
 
 pub struct Uart {
 	interrupting bool
 mut:
-	ua [uart_size]u8
+	ua [size]u8
 }
 
 pub fn (mut u Uart) write(addr u64, value u64, size u8) ! {
